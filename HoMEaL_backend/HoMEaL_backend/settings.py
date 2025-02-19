314952d791e24a596484748c31bc7f78f0a46353
HugoOkumura <hugo-naoki@hotmail.com>
2025-02-19 18:14:40 -0300

    docker

--- HoMEaL_backend/HoMEaL_backend/settings.py
+++ HoMEaL_backend/HoMEaL_backend/settings.py
@@ -22,13 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-7l8eu*@@s&3$7%yy5w(0-%n)3)585(fvj9)v9&e*fd_9o9h3oj'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on in production!d
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0','localhost',]
 
 
-# Application definition
+# Application definitiond
 
 INSTALLED_APPS = [
     'django.contrib.admin',
